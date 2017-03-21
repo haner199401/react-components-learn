435ebb5efbb5d1a083cc8f5d9dbc0e56a42270c8
haner <haner.du@qq.com>
2017-03-21 10:47:52 +0800

    modify index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -10,7 +10,63 @@ import {createHashHistory} from 'history';
 import {App, Home, TodoApp, WikiBox} from './container/App';
 import NotFound from './components/NotFound';
 import todo from './components/Todos/reducer/reducers';
+import './components/style/index.scss';
 
+
+window.store = store;
+
+
+/*
+ App 入口
+ */
+ReactDOM.render((
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Home}/>
+                <Route path="home" component={Home}/>
+                <Route path="topic" component={TodoApp}/>
+                <Route path="wiki" component={WikiBox}/>
+            </Route>
+            <Route path="*" component={NotFound} status={404}/>
+        </Router>
+    </Provider>
+), document.querySelector('#container'));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore, combineReducers} from 'redux';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
+import {createHashHistory} from 'history';
+import {App, Home, TodoApp, WikiBox} from './container/App';
+import NotFound from './components/NotFound';
+import todo from './components/Todos/reducer/reducers';
+import './components/style/index.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore, combineReducers} from 'redux';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
+import {createHashHistory} from 'history';
+import {App, Home, TodoApp, WikiBox} from './container/App';
+import NotFound from './components/NotFound';
+import todo from './components/Todos/reducer/reducers';
+import './components/style/index.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore, combineReducers} from 'redux';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
+import {createHashHistory} from 'history';
+import {App, Home, TodoApp, WikiBox} from './container/App';
+import NotFound from './components/NotFound';
+import todo from './components/Todos/reducer/reducers';
 var store = createStore(combineReducers({
     todo,
     routing: routerReducer
