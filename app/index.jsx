af58c44fe81a2d6f2a69bd9eed3364b57f2293e1
haner <haner.du@qq.com>
2017-03-21 10:54:27 +0800

    修改 bower 目录

--- app/index.jsx
+++ app/index.jsx
@@ -2,8 +2,12 @@ import './components/style/index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {Provider} from 'react-redux'
 import {createStore, combineReducers} from 'redux';
+// import createLogger from 'redux-logger'
+// import thunkMiddleware from 'redux-thunk';
+
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {syncHistoryWithStore, routerReducer} from 'react-router-redux';
 import {createHashHistory} from 'history';
